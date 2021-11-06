fe86049bae5a03c507933cddc28d48691572a2db
Tetiana Rakova <tatyrakova2002@gmail.com>
2021-11-06 10:21:19 +0200

    added button "add product" to products page

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,8 +1,10 @@
 import '../App.css';
+import ProductsForm from '../components/ProductsForm'
 
 const Products = () => (
     <div className='products'>
       <h1>Products</h1>
+      <ProductsForm />
     </div>
 );
 
