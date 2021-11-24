b3b10eddb37636cd530ee8650dc04aaa4be5f6b2
nebosky142 <45599477+OleksiiKoshovyi@users.noreply.github.com>
2021-11-24 18:37:53 +0200

    Add empty product photo and change product card

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import React from 'react';
 import config from '../config'
+import empty from '../empty.png'
 import { Button, Card, Col, Row } from 'react-bootstrap';
 import { Link, Navigate } from "react-router-dom";
 import { PencilFill, TrashFill} from 'react-bootstrap-icons';
@@ -45,41 +46,45 @@ class ProductList extends React.Component<{}, {products: Product[]}> {
       const productList = products.map(p => (
         <Col>
             <Card id={p.id} className="product-area">
-            <Card.Img variant="top" src="https://s3.amazonaws.com/thissnackdoesnotexist.com/tofu-597228_1920.jpg" />
+            <Card.Img variant="top" src={empty} />
             <Card.Body>
-                <Card.Title>{p.name}</Card.Title>
-                <Card.Text>
-                    <Row>
-                        <Col xs={10}>
+                <Card.Title>
+                    <Row xs="auto" className="justify-content-between">
+                        <Col>
+                            {p.name}
+                        </Col>
+                        <Col>
                             <Row>
                                 <Col>
-                                    <div className="p-2 m-1 border rounded">
-                                        Description: {p.description}
-                                    </div>
-                                </Col>
-                            </Row>
-                            <Row xs="auto" className="justify-content-between">
-                                <Col>
-                                    <div className="p-2 m-1 border rounded">
-                                        Unit: {p.unit.name}
-                                    </div>
+                                    <Link to={`edit/${p.id}`}>
+                                        <PencilFill />
+                                    </Link>
                                 </Col>
                                 <Col>
-                                    <div className="p-2 m-1 border rounded">
-                                        Shelf life: {p.shelfLife}
-                                    </div>
+                                    <TrashFill color="red" onClick={e => {this.onClickRemove(p.id)}} />
                                 </Col>
                             </Row>
                         </Col>
-                        <Col xs={2} className="justify-content-between">
-                            <Row className="mb-4">
-                                <Link to={`edit/${p.id}`}>
-                                    <PencilFill />
-                                </Link>
-                            </Row>
-                            <Row className="mt-4">
-                                <TrashFill onClick={e => {this.onClickRemove(p.id)}} />
-                            </Row>
+                    </Row>
+                </Card.Title>
+                <Card.Text>
+                    <Row>
+                        <Col>
+                            <div className="p-2 m-1 border rounded">
+                                Description: {p.description}
+                            </div>
+                        </Col>
+                    </Row>
+                    <Row xs="auto" className="justify-content-between">
+                        <Col>
+                            <div className="p-2 m-1 border rounded">
+                                Unit: {p.unit.name}
+                            </div>
+                        </Col>
+                        <Col>
+                            <div className="p-2 m-1 border rounded">
+                                Shelf life: {p.shelfLife}
+                            </div>
                         </Col>
                     </Row>
                 </Card.Text>
