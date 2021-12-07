58c1ece8c9d7aff8b3d24b56381a16616a928bcd
nebosky142 <45599477+OleksiiKoshovyi@users.noreply.github.com>
2021-12-08 01:57:41 +0200

    Refactor SignUp form: switch to functional

--- src/components/SignUpForm.tsx
+++ src/components/SignUpForm.tsx
@@ -1,15 +1,10 @@
-import axios from 'axios';
-import React from 'react';
-import config from '../config'
+import { useState } from 'react';
 import { Button, Form } from 'react-bootstrap';
-import { Navigate } from 'react-router';
+import { useNavigate } from 'react-router-dom';
 import { signUp } from '../Utils/Api';
 
-interface LooseObject {
-  [key: string]: any
-}
 
-class Input implements LooseObject  {
+class Input {
   name: string = "";
   surname: string = "";
   email: string = "";
@@ -18,7 +13,7 @@ class Input implements LooseObject  {
   agreement: boolean = false;
 }
 
-class Error implements LooseObject {
+class Error {
   confirm_password: string = "";
   message: string = "";
 }
@@ -30,164 +25,121 @@ class NewUserData {
   password: string = "";
 }
 
-class SignUpForm extends React.Component<{}, { input: LooseObject, success: boolean, errors: LooseObject }> {
-  constructor(props: any) {
-    super(props);
-    this.state = {
-      input: new Input(),
-      success: false,
-      errors: new Error()
-    };
+function SignUpForm() {
+  const navigate = useNavigate();
+  const [input, setValues] = useState(new Input())
+  const [errors, setErrors] = useState(new Error())
      
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-     
-  handleChange(event: { target: { name: string | number; value: any; type: string; checked?: boolean }; }) {
-    let input = this.state.input;
-    if (event.target.type == 'checkbox') 
-    {
-      input[event.target.name] = event.target.checked;
-    } else {
-      input[event.target.name] = event.target.value;
-    }
-  
-    this.setState({
-      input: input,
-      errors: new Error()
-    });
-  }
-     
-  async handleSubmit(event: { preventDefault: () => void; }) {
+  const handleSubmit = async (event: { preventDefault: () => void; }) => {
     event.preventDefault();
   
-    if(this.validate()){
-      const data: NewUserData = this.state.input as Input
-      const errors = new Error();
-      let success = false;
+    if(validate()){
+      const data: NewUserData = input as Input
 
       const result = await signUp(data);
       if (result.error) {
-        errors.message = result.error;
+        setErrors({confirm_password: '', message: result.error})
       } else {
-        success = true;
+        navigate('/signin')
       }
       
-      const input = new Input();
-      this.setState({input: input, success: success, errors: errors});
+      setValues(new Input())
     }
   }
   
-  validate(){
-      let input = this.state.input;
-      let errors = new Error();
+  const validate = () =>{
       let isValid = true;
-  
-      if (typeof input["password"] !== "undefined" && typeof input["confirm_password"] !== "undefined") {
-          
-        if (input["password"] != input["confirm_password"]) {
+      if (typeof input.password !== "undefined" && typeof input.confirm_password !== "undefined") {
+        if (input.password != input.confirm_password) {
           isValid = false;
-          errors["confirm_password"] = "Passwords don't match.";
+          setErrors({message: '', confirm_password: "Passwords don't match."})
         }
-      } 
-  
-      this.setState({
-        errors: errors
-      });
-  
+      }  
       return isValid;
   }
      
-  render() {
-    const error = this.state.errors;
-    const success = this.state.success;
-    return (
-      <Form onSubmit={this.handleSubmit}>
-        {error.message && (
-          <div className="text-danger">
-            <h6>{error.message}</h6>
-          </div>
-        ) || success && (
-          <Navigate to="/signin" replace={true} />
-        )}
-        <Form.Group className="mb-3" controlId="formBasicName">
-          <Form.Label>Name</Form.Label>
-          <Form.Control
-            id="name"
-            name="name"
-            value={this.state.input.name}
-            onChange={this.handleChange}
-            required
-            type="text"
-            placeholder="Enter name"
-          />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicSurname">
-          <Form.Label>Surnme</Form.Label>
-          <Form.Control
-            id="surname"
-            name="surname"
-            value={this.state.input.surname}
-            onChange={this.handleChange}
-            required
-            type="text"
-            placeholder="Enter surname"
-          />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control
-            id="email"
-            name="email"
-            value={this.state.input.email}
-            onChange={this.handleChange}
-            required
-            type="email"
-            placeholder="Enter email" />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            id="password"
-            name="password"
-            value={this.state.input.password}
-            onChange={this.handleChange}
-            required
-            type="password"
-            placeholder="Enter password"
-          />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicConfirmPassword">
-          <Form.Label>Confirm password</Form.Label>
-          <Form.Control
-            id="confirm_password"
-            name="confirm_password"
-            value={this.state.input.confirm_password}
-            onChange={this.handleChange}
-            required
-            type="password"
-            placeholder="Enter password again"
-          />
-          <div className="text-danger">{this.state.errors.confirm_password}</div>
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicCheckbox">
-          <Form.Check
-            checked={this.state.input.agreement}
-            onChange={this.handleChange}
-            required
-            name="agreement"
-            type="checkbox"
-            label="I agree with terms and conditions" />
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>      
-      </Form>
-    );
-  }
+  return (
+    <Form onSubmit={handleSubmit}>
+      <div className="text-danger">
+        <h6>{errors.message}</h6>
+      </div>
+      <Form.Group className="mb-3" controlId="formBasicName">
+        <Form.Label>Name</Form.Label>
+        <Form.Control
+          id="name"
+          name="name"
+          value={input.name}
+          onChange={(e) => {setValues({...input, name: e.target.value})}}
+          required
+          type="text"
+          placeholder="Enter name"
+        />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicSurname">
+        <Form.Label>Surname</Form.Label>
+        <Form.Control
+          id="surname"
+          name="surname"
+          value={input.surname}
+          onChange={(e) => {setValues({...input, surname: e.target.value})}}
+          required
+          type="text"
+          placeholder="Enter surname"
+        />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control
+          id="email"
+          name="email"
+          value={input.email}
+          onChange={(e) => {setValues({...input, email: e.target.value})}}
+          required
+          type="email"
+          placeholder="Enter email" />
+        <Form.Text className="text-muted">
+          We'll never share your email with anyone else.
+        </Form.Text>
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control
+          id="password"
+          name="password"
+          value={input.password}
+          onChange={(e) => {setValues({...input, password: e.target.value})}}
+          required
+          type="password"
+          placeholder="Enter password"
+        />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicConfirmPassword">
+        <Form.Label>Confirm password</Form.Label>
+        <Form.Control
+          id="confirm_password"
+          name="confirm_password"
+          value={input.confirm_password}
+          onChange={(e) => {setValues({...input, confirm_password: e.target.value})}}
+          required
+          type="password"
+          placeholder="Enter password again"
+        />
+        <div className="text-danger">{errors.confirm_password}</div>
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicCheckbox">
+        <Form.Check
+          checked={input.agreement}
+          onChange={(e) => {setValues({...input, agreement: e.target.checked})}}
+          required
+          name="agreement"
+          type="checkbox"
+          label="I agree with terms and conditions" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>      
+    </Form>
+  );
 }
   
 export default SignUpForm;
