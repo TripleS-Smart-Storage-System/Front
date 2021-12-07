12460e1d2479a3ce2e09d274e4d9f73611ed7b3d
Tetiana Rakova <tatyrakova2002@gmail.com>
2021-12-07 18:17:59 +0200

    Refactor SignIn page : switch to functional

--- src/components/SignInForm.tsx
+++ src/components/SignInForm.tsx
@@ -1,18 +1,13 @@
-import axios from 'axios';
-import React from 'react';
-import config from '../config'
-import { Button, Form } from 'react-bootstrap';
-import { Navigate } from 'react-router-dom';
-import { setUserSession } from '../Utils/Common';
-import { signIn } from '../Utils/Api';
+import axios from "axios";
+import React, { Dispatch, SetStateAction, useEffect, useState } from "react";
+import config from "../config";
+import { Button, Form } from "react-bootstrap";
+import { Navigate, useNavigate } from "react-router-dom";
+import { Unit } from "../types";
+import { signIn, getUnits } from "../Utils/Api";
 
 interface LooseObject {
-  [key: string]: any
-}
-
-class Input implements LooseObject  {
-  email: string = "";
-  password: string = "";
+  [key: string]: any;
 }
 
 class Error {
@@ -20,97 +15,80 @@ class Error {
   status: number = 0;
 }
 
-class NewUserData {
+class Input {
   email: string = "";
   password: string = "";
 }
 
-class SignInForm extends React.Component<{}, { input: LooseObject, success: boolean, errors: LooseObject }> {
-  constructor(props: any) {
-    super(props);
-    this.state = {
-      input: new Input(),
-      success: false,
-      errors: new Error()
-    };
-     
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-     
-  handleChange(event: { target: { name: string | number; value: any; type: string; checked?: boolean }; }) {
-    let input = this.state.input;
-    input[event.target.name] = event.target.value;
-  
-    this.setState({
-      input: input,
-      errors: new Error()
-    });
-  }
-     
-  async handleSubmit(event: { preventDefault: () => void; }) {
+type SignInData = Input;
+
+function SignInForm() {
+  const navigate = useNavigate();
+  const [input, setValues] = useState(new Input());
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    async function fetchApi() {
+      setValues(input);
+    }
+
+    fetchApi();
+  }, []);
+
+  const handleSubmit = async (event: { preventDefault: () => void }) => {
     event.preventDefault();
 
-    const data: NewUserData = this.state.input as Input
-    const errors = new Error();
-    let success = false;
-
+    const data: SignInData = input as Input;
     const result = await signIn(data);
-    if (result.error) {
-      errors.message = result.error;
-      errors.status = 0;
-    } else {
-      success = true;
-      const token = result.response?.data.token ?? '';
-      const user = result.response?.data.id ?? '';
-      setUserSession(token, user)
+    setError(result?.error ?? "");
+
+    const token = result.response?.data.token ?? "";
+    const user = result.response?.data.id ?? "";
+    // setUserSession(token, user)
+
+    if (user) {
+      navigate("/");
     }
-    
-    const input = new Input();
-    this.setState({input: input, success: success, errors: errors});
-  }
-     
-  render() {
-    const error = this.state.errors;
-    const success = this.state.success;
-    return (
-      <Form onSubmit={this.handleSubmit}>
-        {error.message && (
-          <div className="text-danger">
-            <h6>{error.message}</h6>
-          </div>
-        ) || success && (
-          <Navigate to="/" replace={true} />
-        )}
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control
-            id="email"
-            name="email"
-            value={this.state.input.email}
-            onChange={this.handleChange}
-            required
-            type="email"
-            placeholder="Enter email" />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            id="password"
-            name="password"
-            value={this.state.input.password}
-            onChange={this.handleChange}
-            required
-            type="password"
-            placeholder="Enter password"
-          />
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>      
-      </Form>
-    );
-  }
+  };
+
+  return (
+    <Form onSubmit={handleSubmit} action="/">
+      <div className="text-danger">
+        <h6>{error}</h6>
+      </div>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control
+          id="email"
+          name="email"
+          value={input.email}
+          onChange={(event) => {
+            setValues({ ...input, email: event.target.value });
+          }}
+          required
+          type="email"
+          placeholder="Enter email"
+        />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control
+          id="password"
+          name="password"
+          value={input.password}
+          onChange={(event) => {
+            setValues({ ...input, password: event.target.value });
+          }}
+          required
+          type="password"
+          placeholder="Enter password"
+        />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+  );
 }
-  
+
 export default SignInForm;
