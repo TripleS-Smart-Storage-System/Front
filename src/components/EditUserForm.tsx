15253a30d5527501c79df13829c3fad0d6bbd323
nebosky142 <45599477+OleksiiKoshovyi@users.noreply.github.com>
2021-12-08 03:01:59 +0200

    Refactor EditUserForm: switch to functional

--- src/components/EditUserForm.tsx
+++ src/components/EditUserForm.tsx
@@ -1,23 +1,10 @@
-import axios from 'axios';
-import React from 'react';
-import config from '../config'
+import { useLayoutEffect, useState } from 'react';
 import { Button, Form } from 'react-bootstrap';
-import { Navigate } from "react-router-dom";
-import { User, Unit } from '../types';
-import { truncateSync } from 'fs';
-import { couldStartTrivia } from 'typescript';
+import { useNavigate } from "react-router-dom";
+import { User } from '../types';
 import { getUser, updateUser } from '../Utils/Api';
 
-interface LooseObject {
-  [key: string]: any
-}
-
-class Error {
-  message: string = "";
-  status: number = 0;
-}
-
-class Input implements LooseObject {
+class Input {
   name: string = "";
   surName: string = "";
   nickName: string = "";
@@ -27,130 +14,88 @@ class EditUserData extends Input {
   id: string = "";
 }
 
-class EditProducForm extends React.Component<{userId: string}, { userId: string, input: LooseObject, units: Unit[], success: boolean, errors: Error}> {
-  constructor(props: {userId: string}) {
-    super(props);
-    this.state = {
-      userId: props.userId,
-      input: new Input(),
-      units: new Array<Unit>(),
-      success: false,
-      errors: new Error()
-    };
-    
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSelectedChange = this.handleSelectedChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+function EditProducForm(props: {userId: string}) {
+  const userId = props.userId
+  const navigate = useNavigate();
+  const [input, setValues] = useState(new Input())
+  const [error, setError] = useState('')
 
-  async componentDidMount() {
-    const input = this.state.input;
-    const userId = this.state.userId;
-    const user: User = await getUser(userId);
-    const errors = new Error()
-    if (user.id != userId) {
-      errors.message = "Something went wrong"
-    } else {
-      input.name = user.name;
-      input.surName = user.surName;
-      input.nickName = user.nickName;
+  useLayoutEffect(() => {
+    async function fetchApi() {
+      const user: User = await getUser(userId);
+      if (user.id != userId) {
+        setError("Something went wrong")
+      } else {
+        setValues({
+          name: user.name,
+          surName: user.surName,
+          nickName: user.nickName
+        })
+      }
     }
-    this.setState({input: input, errors: errors});
-  }
+    fetchApi()
+  }, [])
      
-  handleChange(event: { target: { name: string | number; value: any }; }) {
-    let input = this.state.input;
-    input[event.target.name] = event.target.value.toString();
-
-    this.setState({
-      input: input
-    });
-  }
-
-  handleSelectedChange(event: { target: any; }) {
-    let input = this.state.input;
-    input.unitId = event.target.value;
-
-    this.setState({
-      input: input
-    });
-  }
-     
-  async handleSubmit(event: { preventDefault: () => void; }) {
+  const handleSubmit = async (event: { preventDefault: () => void; }) => {
     event.preventDefault();
 
-    const data: EditUserData = this.state.input as EditUserData;
-    data.id = (this.props.userId).toString();
+    const data: EditUserData = input as EditUserData;
+    data.id = (props.userId).toString();
 
-    const error = new Error();
     const result = await updateUser(data);
-    error.message = result?.error ?? '';
-    let success = error.message.length == 0;
-
-    this.setState({errors: error, success: success})
+    const error = result?.error ?? '';
+    if (error) {
+      setError(error)
+    } else {
+      navigate('/users')
+    }
   }
-     
-  render() {
-    const unitId = this.state.input.unitId;
-    const units = this.state.units;
-    const index = units.findIndex(u => u.id == unitId);
-    const unitOptions: JSX.Element[] = units.map((unit) =>
-      <option selected={unitId == unit.id} value={unit.id} id={unit.id} key={unit.name}>{unit.name}</option>
-    );
-
-    const success = this.state.success;
-    const error = this.state.errors;
-    
-    return (
-      <Form onSubmit={this.handleSubmit} action="/users">
-        {error.message && (
-          <div className="text-danger">
-            <h6>{error}</h6>
-          </div>
-        ) || success && (
-          <Navigate to="/users" replace={true} />
-        )}
-          <Form.Group className="mb-3" controlId="formBasicName">
-          <Form.Label>Name</Form.Label>
-          <Form.Control
-            id="name"
-            name="name"
-            value={this.state.input.name}
-            onChange={this.handleChange}
-            required
-            type="text"
-            placeholder="Enter name"
-          />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicSurname">
-          <Form.Label>Surnme</Form.Label>
-          <Form.Control
-            id="surName"
-            name="surName"
-            value={this.state.input.surName}
-            onChange={this.handleChange}
-            required
-            type="text"
-            placeholder="Enter surName"
-          />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicNickname">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control
-            id="nickName"
-            name="nickName"
-            value={this.state.input.nickName}
-            onChange={this.handleChange}
-            required
-            type="text"
-            placeholder="Enter nickname" />
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>      
-      </Form>
-    );
-  }
+      
+  return (
+    <Form onSubmit={handleSubmit} action="/users">
+      <div className="text-danger">
+        <h6>{error}</h6>
+      </div>
+      <Form.Group className="mb-3" controlId="formBasicName">
+        <Form.Label>Name</Form.Label>
+        <Form.Control
+          id="name"
+          name="name"
+          value={input.name}
+          onChange={e => {setValues({...input, name: e.target.value});}}
+          required
+          type="text"
+          placeholder="Enter name"
+        />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicSurname">
+        <Form.Label>Surnme</Form.Label>
+        <Form.Control
+          id="surName"
+          name="surName"
+          value={input.surName}
+          onChange={e => {setValues({...input, surName: e.target.value});}}
+          required
+          type="text"
+          placeholder="Enter surName"
+        />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicNickname">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control
+          id="nickName"
+          name="nickName"
+          value={input.nickName}
+          onChange={e => {setValues({...input, nickName: e.target.value});}}
+          required
+          type="text"
+          placeholder="Enter nickname" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>      
+    </Form>
+  );
 }
   
 export default EditProducForm;
